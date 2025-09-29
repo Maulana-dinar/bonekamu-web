c5dcdaf5fec5fb45233dccf9d490db34173bdf49
Maulana-dinar <dinarmaulana2803@gmail.com>
2025-09-29 07:24:28 +0700

    p

--- page/home.php
+++ page/home.php
@@ -12,19 +12,7 @@
             <a href="boneka3.html"><button type="button"class="buton button">Lebih Detail</button></a>
           </div>
         </div>
-        <div class="col bunga">
-          <div class="card product-card">
-            <img src="image/bunga_1.png" alt="Adidas Samba" class="card-img-top">
-            <div class="card-body product-info">
-              <h6>BUNGA</h6>
-              <p>KUBAH TERPESONA - MERAH BERGAIRAH</p>
-              <p class="  ketharga fs-6 text-decoration-line-through">$122,10 USD</p>
-              <p class="ketdiskon">( -28%)</p>
-              <p class="keterharga fs-5 ">Rp. 1.435.000</p>
-              <a href="bunga1.html"><button type="button"class="buton button">Lebih Detail</button></a>
-            </div>
-          </div>
-        </div>
+        
       </div>
       <div class="col">
         <div class="card product-card">
@@ -38,19 +26,33 @@
             <a href="boneka2.html"><button type="button"class="buton button">Lebih Detail</button></a>
           </div>
         </div>    
-        <div class="col bunga">
-          <div class="card product-card">
-            <img src="image/bunga 2.png" alt="Adidas Samba" class="card-img-top">
+       
+      </div>
+      <div class="col">
+        <div class="card product-card">
+          <img src="image/boneka1.jpg" alt="Adidas Samba" class="card-img-top">
             <div class="card-body product-info">
-              <h6>BUNGA</h6>
-              <p>ASTORIA BARU - MERAH BERGAIRAH</p>
-              <p class="  ketharga fs-6 text-decoration-line-through">$80,22 USD</p>
-              <p class="ketdiskon">( -25%)</p>
-              <p class="keterharga fs-5 ">Rp. 985.000</p>
-              <a href="bunga2.html"><button type="button"class="buton button">Lebih Detail</button></a>
-            </div>
-          </div>      
-        </div>
+            <h6>BERUANG</h6>
+            <p>STUFFED PANDA ANIMAL COSTUME</p>
+            <p class="  ketharga fs-6 text-decoration-line-through">$18,32 USD   </p>
+            <p class="ketdiskon">( -4%)</p>
+            <p class="keterharga fs-5 ">Rp. 300.000</p>
+            <a href="boneka1.html"><button type="button"class="buton button">Lebih Detail</button></a>
+          </div>
+        </div>           
+      </div>
+      <div class="col">
+        <div class="card product-card">
+          <img src="image/boneka 4.webp" alt="" class="card-img-top">
+            <div class="card-body product-info">
+            <h6>BERUANG</h6>
+            <p>BANTAL KOREAN PENGUIN TERBARU / BONEKA MEWAH SIZE 30 cm</p>
+            <p class="  ketharga fs-6 text-decoration-line-through">$18,32 USD   </p>
+            <p class="ketdiskon">( -4%)</p>
+            <p class="keterharga fs-5 ">Rp. 300.000</p>
+            <a href="boneka1.html"><button type="button"class="buton button">Lebih Detail</button></a>
+          </div>
+        </div>    
       </div>
       <div class="col">
         <div class="card product-card">
@@ -64,27 +66,78 @@
             <a href="boneka1.html"><button type="button"class="buton button">Lebih Detail</button></a>
           </div>
         </div>    
-       
-        <div class="col bunga">
-          <div class="card product-card">
-            <img src="image/bunga 3.png" alt="Adidas Samba" class="card-img-top">
-            <div class="card-body product-info">
-              <h6>BUNGA</h6>
-              <p>IMITASI TULIP PU</p>
-              <p class="  ketharga fs-6 text-decoration-line-through">$62 USD   </p>
-              <p class="ketdiskon">( -25%)</p>
-              <p class="keterharga fs-5 ">Rp. 7.500</p>
-              <a href="bunga3.html"><button type="button"class="buton button">Lebih Detail</button></a>
-            </div>
-          </div>   
-        </div>
       </div>
-      <div class="col">
-        
-      </div>
-      <div class="col">
 
-      </div>
       
+      
+<div class="container text-center">
+  <div class="row align-items-center">
+     <div class="col bunga">
+        <div class="card product-card">
+          <img src="image/bunga_1.png" alt="Adidas Samba" class="card-img-top">
+          <div class="card-body product-info">
+            <h6>BUNGA</h6>
+            <p>KUBAH TERPESONA - MERAH BERGAIRAH</p>
+            <p class="  ketharga fs-6 text-decoration-line-through">$122,10 USD</p>
+            <p class="ketdiskon">( -28%)</p>
+            <p class="keterharga fs-5 ">Rp. 1.435.000</p>
+            <a href="bunga1.html"><button type="button"class="buton button">Lebih Detail</button></a>
+          </div>
+        </div>
+     </div>
+     <div class="col bunga">
+        <div class="card product-card">
+          <img src="image/bunga 2.png" alt="Adidas Samba" class="card-img-top">
+          <div class="card-body product-info">
+            <h6>BUNGA</h6>
+            <p>ASTORIA BARU - MERAH BERGAIRAH</p>
+            <p class="  ketharga fs-6 text-decoration-line-through">$80,22 USD</p>
+            <p class="ketdiskon">( -25%)</p>
+            <p class="keterharga fs-5 ">Rp. 985.000</p>
+            <a href="bunga2.html"><button type="button"class="buton button">Lebih Detail</button></a>
+          </div>
+        </div>      
+      </div>
+    <div class="col bunga">
+      <div class="card product-card">
+        <img src="image/bunga 3.png" alt="Adidas Samba" class="card-img-top">
+        <div class="card-body product-info">
+          <h6>BUNGA</h6>
+          <p>IMITASI TULIP PU</p>
+          <p class="  ketharga fs-6 text-decoration-line-through">$62 USD   </p>
+          <p class="ketdiskon">( -25%)</p>
+          <p class="keterharga fs-5 ">Rp. 7.500</p>
+          <a href="bunga3.html"><button type="button"class="buton button">Lebih Detail</button></a>
+        </div>
+      </div>   
+     </div>
+    <div class="col bunga">
+      <div class="card product-card">
+        <img src="image/bunga 3.png" alt="Adidas Samba" class="card-img-top">
+        <div class="card-body product-info">
+          <h6>BUNGA</h6>
+          <p>IMITASI TULIP PU</p>
+          <p class="  ketharga fs-6 text-decoration-line-through">$62 USD   </p>
+          <p class="ketdiskon">( -25%)</p>
+          <p class="keterharga fs-5 ">Rp. 7.500</p>
+          <a href="bunga3.html"><button type="button"class="buton button">Lebih Detail</button></a>
+        </div>
+      </div>    
+    </div>
+    <div class="col bunga">
+      <div class="card product-card">
+        <img src="image/bunga 3.png" alt="Adidas Samba" class="card-img-top">
+        <div class="card-body product-info">
+          <h6>BUNGA</h6>
+          <p>IMITASI TULIP PU</p>
+          <p class="  ketharga fs-6 text-decoration-line-through">$62 USD   </p>
+          <p class="ketdiskon">( -25%)</p>
+          <p class="keterharga fs-5 ">Rp. 7.500</p>
+          <a href="bunga3.html"><button type="button"class="buton button">Lebih Detail</button></a>
+        </div>
+      </div>      
+    </div>
+  </div>
+</div>
     </div>
 </div>  
