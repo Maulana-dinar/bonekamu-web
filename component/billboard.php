c523040a67bb04c736f34fc47fd6de2103e61279
Maulana-dinar <dinarmaulana2803@gmail.com>
2026-04-23 20:50:18 +0700

    nice

--- component/billboard.php
+++ component/billboard.php
@@ -1,37 +1,52 @@
-<div>
-  <p class="top fw-bold text-center fs-2">Temukan boneka hewan kesayangan anda hanya di Bonekamu</p> 
-  <p class="text-center fs-4 atas">
-    Hanya di toko bonekamu anda bisa mecari boneka hingga bunga impian <br> 
-    anda yang menarik serta memikat hati
-  </p>  
-</div>  
-
-<div class="container text-center billboard">
-  <div class="row align-items-start">
+<style>
+  .billboard{
+    padding-bottom: 15vh;
+  }
+  h1 {
+    margin-top :14vh;
+  }
+</style>
+<div class="container billboard text-center">
+  <div class="row">
     <div class="col">
-      <div class="gambarantengah">
-        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img src="image/gambar tengah 1.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="image/gambar tengah 2.avif" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-              <img src="image/gambar tengah 3.webp" class="d-block w-100" alt="...">
+    <div>
+       <h1 class="top fw-bold text-center fs-2">Temukan boneka hewan kesayangan anda hanya di Bonekamu</h1> 
+       <h3 class="text-center fs-4 atas">
+         Hanya di toko bonekamu anda bisa mecari boneka hingga bunga impian <br> 
+          anda yang menarik serta memikat hati
+       </h3>  
+    </div>  
+    </div>
+    <div class="col">
+      <div class="container text-center">
+        <div class="row align-items-start">
+          <div class="col">
+            <div class="gambarantengah">
+              <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+               <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="image/gambar tengah 1.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="image/gambar tengah 2.avif" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="image/gambar tengah 3.webp" class="d-block w-100" alt="...">
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+             </div>
             </div>
           </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-        </div>
-      </div>
+        </div>    
+      </div>  
     </div>
-  </div>    
-</div>  
+  </div>
+</div>
--- component/footer.php
+++ component/footer.php
@@ -1,6 +1,87 @@
-<footer class="footer">
+<style>
+    .foooter {
+    background-color: #c6914667;
+    color: #fff;
+    padding: 60px 80px 20px;
+    font-family: 'Poppins', sans-serif;
+    margin-top: 15vh;
+}
+
+.footer-container {
+    display: flex;
+    justify-content: space-between;
+    gap: 60px;
+}
+
+.footer-left {
+    max-width: 300px;
+}
+
+.logo {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    font-weight: 600;
+    font-size: 18px;
+    margin-bottom: 15px;
+}
+
+.logo img {
+    width: 40px; /* ukuran logo */
+    height: auto;
+}
+
+.footer-left p {
+    font-size: 13px;
+    color: #000000;
+    line-height: 1.6;
+}
+
+.socials {
+    margin-top: 15px;
+}
+
+.socials a {
+    margin-right: 10px;
+}
+
+.circle {
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    background: #fff;
+    border-radius: 50%;
+}
+
+.footer-middle h4,
+.footer-right h4 {
+    font-size: 16px;
+    margin-bottom: 15px;
+}
+
+.footer-middle ul,
+.footer-right ul {
+    list-style: none;
+    padding: 0;
+    font-size: 13px;
+    color: #000000;
+}
+
+.footer-middle li,
+.footer-right li {
+    margin-bottom: 10px;
+}
+
+.footer-bottom {
+    text-align: center;
+    margin-top: 40px;
+    font-size: 12px;
+    color: #000000;
+}
+
+</style>
+<footer class="foooter">
     <div class="footer-container">
-        
         <!-- LEFT -->
         <div class="footer-left">
             <div class="logo">
